dc3e3300db8fa005832c058bf1c114da75931d24
Anna Dunster <anna@annardunster.com>
2024-05-11 17:34:48 -0700

    skeleton some noise function

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub fn some_noise_function(position: i32) -> u32 {
+    println!("Called some_noise_function");
+    let mut mangled = position as u32;
+
+    return mangled;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use eiserloh_rng_example::*;
+
 fn main() {
     println!("Hello, world!");
+    let input = 42;
+    let result = some_noise_function(input);
+    println!("Got noise function result: {}", result)
 }
