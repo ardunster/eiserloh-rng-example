10e6572c67313591523b87e3bca060c8139b2852
Anna Dunster <anna@annardunster.com>
2024-05-11 17:46:05 -0700

    overflow multiplication stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
+/// The fourth [Bell Prime](https://en.wikipedia.org/wiki/Bell_number#Bell_primes), because Wikipedia.
+const SOME_BIG_PRIME_NUMBER: u32 = 27_644_437;
+/// The answer to every question that isn't 42.
+const SOME_OTHER_NUMBER: u32 = 17;
+
 pub fn some_noise_function(position: i32) -> u32 {
     println!("Called some_noise_function");
     let mut mangled = position as u32;
+    mangled = mangled.wrapping_mul(SOME_BIG_PRIME_NUMBER);
+    mangled = mangled.wrapping_add(SOME_OTHER_NUMBER);
 
-    return mangled;
+    mangled
 }
